1a12f19c2251bac996b88ecb195b01b44d15003b
Bruna <brualmeida111@gmail.com>
2023-09-18 12:05:00 +0100

    [mudança] mudança de authorship

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,11 @@
 __author__ = "João Correia"
+__coauthor__ = "Bruna Almeida"
 __credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "João Correia"
 __email__ = "jfscorreia95@gmail.com"
+__coauthoremail__ = "pg51187@uminho.pt"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
